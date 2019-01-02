3eee9a7561afe909ff2b985451e9a80ce3ec9e9d
Levi <levi_blodgett@outlook.com>
2019-01-01 20:17:08 -0600

    Started local storage functionality, updated text field to #

--- appes6.js
+++ appes6.js
@@ -96,6 +96,25 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks() {
+    
+  }
+
+  static displayBooks() {
+    
+  }
+
+  static addBook() {
+
+  }
+
+  static removeBook() {
+
+  }
+}
+
 // Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
   // Get form values
